b0006f62ff4804a78c27f655430b13d65315e20c
grainsize <81246964+grainsize@users.noreply.github.com>
2025-07-24 16:28:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -66,11 +66,11 @@ descriptions = {
 }
 
 images = {
-    "테토남": "https://images.unsplash.com/photo-1610563166154-9b7be0b11a06?auto=format&fit=crop&w=1200&q=80",
-    "에겐남": "https://images.unsplash.com/photo-1594824476967-48c8b9642731?auto=format&fit=crop&w=1200&q=80",
-    "테토녀": "https://images.unsplash.com/photo-1586281380381-52c3ea5484d2?auto=format&fit=crop&w=1200&q=80",
-    "에겐녀": "https://images.unsplash.com/photo-1617038491894-5b18d376ef1b?auto=format&fit=crop&w=1200&q=80",
-    "혼합형": "https://images.unsplash.com/photo-1534438327276-14e5300c3a48?auto=format&fit=crop&w=1200&q=80"
+    "테토남": "images/Teto_male.jpg",
+    "에겐남": "images/Egen_male.jpg",
+    "테토녀": "images/Teto_female.jpg",
+    "에겐녀": "images/Egen_female.jpg",
+    "혼합형": "images/Balanced.jpg"
 }
 
 # 스타일 삽입
